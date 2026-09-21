62bd9c1a4bb8647539f3129fd2c1c362431b423c
jz <gdga@cn.com>
2026-09-21 14:19:31 +0800

    docs(header): add complete playground demo

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,37 +1,16 @@
 <template>
   <div class="playground-scroll">
-    <!-- <MegaMenuDemo />
-    <myMegaMenu></myMegaMenu> -->
-    <!-- <div class="demo-divider" />
-    <MySearchBarDemo />
-    <div class="demo-divider" />
-    <TablePaginationDemo /> -->
-
-    <!-- <DialogDemo></DialogDemo> -->
-
-    <!-- <MySearchBarDemo></MySearchBarDemo> -->
-
-    <AsideMenuDemo></AsideMenuDemo>
+    <HeaderDemo />
   </div>
 </template>
 
 <script setup lang="ts">
-import MegaMenuDemo from './demos/MegaMenuDemo.vue'
-import myMegaMenu from './demos/myMegaMenu.vue'
-import AsideMenuDemo from './demos/AsideMenuDemo.vue'
-import TablePaginationDemo from './demos/TablePaginationDemo.vue'
-import MySearchBarDemo from './demos/MySearchBarDemo.vue'
-import DialogDemo from './demos/DialogDemo.vue'
+import HeaderDemo from './demos/HeaderDemo.vue'
 </script>
 
 <style scoped>
 .playground-scroll {
   height: 100%;
   overflow: auto;
-}
-
-.demo-divider {
-  height: 1px;
-  background: #cfd6df;
 }
 </style>
